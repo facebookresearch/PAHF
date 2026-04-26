9356984b264fcacce3fe9c65a9267c71c8f7eeec
Saghar Hosseini <saghar.hossaini@gmail.com>
2026-04-26 01:11:57 -0700

    Update agent_utils.py

--- utils/agent_utils.py
+++ utils/agent_utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 """
 Shared utility functions for embodied_agent.py and shopping_agent.py
 Contains common functionality to reduce code duplication.
