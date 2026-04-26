1cfc6a0d4428c04bd5ac83a8a217c64838b95312
Saghar Hosseini <saghar.hossaini@gmail.com>
2026-04-26 01:11:49 -0700

    Update shopping_prompts.py

--- prompts/shopping_prompts.py
+++ prompts/shopping_prompts.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 mc_gen_prompt_template = """You are simulating an AI shopping agent that helps users buy products in an online shopping system. The agent can select from various product options or decline to purchase if none meet requirements.
 
 PRODUCT CATEGORIES:
