a33752fc06b552fbd14db9f3e905a497ffcbba3a
Saghar Hosseini <saghar.hossaini@gmail.com>
2026-04-26 01:11:41 -0700

    Update embodied_prompts.py

--- prompts/embodied_prompts.py
+++ prompts/embodied_prompts.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 mc_gen_prompt_template = """You are simulating a robot operating in an office environment with access to multiple rooms and areas:
 
 KITCHEN AREAS:
